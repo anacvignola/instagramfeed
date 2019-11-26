13eaef759fb2e01e1bc3b4333cd3b0b5cc54aa20
anacvignola <anacvignola@riseup.net>
2019-11-26 00:05:40 -0300

    add mobile and details

--- mobile/src/pages/New.js
+++ mobile/src/pages/New.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import api from '../services/api';
 import ImagePicker from 'react-native-image-picker';
 import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
 import {TextInput} from 'react-native-gesture-handler';
@@ -9,6 +10,8 @@ export default class New extends Component {
   };
 
   state = {
+    preview: null,
+    image: null,
     author: '',
     place: '',
     description: '',
@@ -20,9 +23,52 @@ export default class New extends Component {
       {
         title: 'Selecionar imagem',
       },
-      upload => {},
+      upload => {
+        if (upload.error) {
+          console.log('Error');
+        } else if (upload.didCancel) {
+          console.log('Used canceled');
+        } else {
+          const preview = {
+            uri: `data:image/jpeg;base64,${upload.data}`,
+          }
+
+          let prefix;
+          let ext;
+
+          if (upload.fileName) {
+            [prefix, ext] = upload.fileName.split('.')
+            ext = ext.toLowerCase() === 'heic' ? 'jpg' : ext;
+          } else {
+            prefix = new DataCue().getTime();
+            ext = 'jpg';
+          }
+          
+          const image = {
+            uri: upload.uri,
+            type: upload.type,
+            name: `${prefix}.${ext}`
+          };
+
+          this.setState({ preview, image });
+        }
+      },
     );;
   };
+
+  handleSubmit = async () => {
+    const data = new FormData();
+
+    data.append('image', this.state.image);
+    data.append('author', this.state.author);
+    data.append('place', this.state.place);
+    data.append('description', this.state.description);
+    data.append('hashtags', this.state.hashtags);
+
+    await api.post('posts', data);
+
+    this.props.navigation.navigate('Feed');
+  }
 
   render() {
     return (
@@ -32,6 +78,7 @@ export default class New extends Component {
           onPress={this.handleSelectImage}>
           <Text style={styles.selectButtonText}>Selecionar imagem</Text>
         </TouchableOpacity>
+        { this.state.preview && <Image style={styles.preview} source={this.state.preview} />}
         <TextInput
           style={styles.input}
           autoCorrect={false}
@@ -68,7 +115,7 @@ export default class New extends Component {
           value={this.state.hashtags}
           onChangeText={hashtags => this.setState({hashtags})}
         />
-        <TouchableOpacity style={styles.shareButton} onPress={() => {}}>
+        <TouchableOpacity style={styles.shareButton} onPress={this.handleSubmit}>
           <Text style={styles.shareButtonText}>Compartilhar</Text>
         </TouchableOpacity>
       </View>
